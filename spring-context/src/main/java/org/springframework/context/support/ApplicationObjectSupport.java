f3e71ad31897fb95937fad580ef1f6c9e1184d74
qinziyue <1252525229@qq.com>
2018-11-14 12:31:29 +0800

    初始化所有项目，后面再清理多余的文件

---
+++ spring-context/src/main/java/org/springframework/context/support/ApplicationObjectSupport.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.context.support;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+/**
+ * Convenient superclass for application objects that want to be aware of
+ * the application context, e.g. for custom lookup of collaborating beans
+ * or for context-specific resource access. It saves the application
+ * context reference and provides an initialization callback method.
+ * Furthermore, it offers numerous convenience methods for message lookup.
+ *
+ * <p>There is no requirement to subclass this class: It just makes things
+ * a little easier if you need access to the context, e.g. for access to
+ * file resources or to the message source. Note that many application
+ * objects do not need to be aware of the application context at all,
+ * as they can receive collaborating beans via bean references.
+ *
+ * <p>Many framework classes are derived from this class, particularly
+ * within the web support.
+ *
+ * @author Rod Johnson
+ * @author Juergen Hoeller
+ * @see org.springframework.web.context.support.WebApplicationObjectSupport
+ */
+public abstract class ApplicationObjectSupport implements ApplicationContextAware {
+
+	/**
+	 * Logger that is available to subclasses.
+	 */
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	/**
+	 * ApplicationContext this object runs in.
+	 */
+	@Nullable
+	private ApplicationContext applicationContext;
+
+	/**
+	 * MessageSourceAccessor for easy message access.
+	 */
+	@Nullable
+	private MessageSourceAccessor messageSourceAccessor;
+
+
+	/**
+	 * ApplicationContextAware接口的实现方法，通过此方法控制ApplicationContext初始化
+	 *
+	 * @param context
+	 * @throws BeansException
+	 */
+	@Override
+	public final void setApplicationContext(@Nullable ApplicationContext context) throws BeansException {
+		if (context == null && !isContextRequired()) {
+			// Reset internal context state.
+			this.applicationContext = null;
+			this.messageSourceAccessor = null;
+		} else if (this.applicationContext == null) {
+			// Initialize with passed-in context.
+			if (!requiredContextClass().isInstance(context)) {
+				throw new ApplicationContextException(
+						"Invalid application context: needs to be of type [" + requiredContextClass().getName() + "]");
+			}
+			this.applicationContext = context;
+			this.messageSourceAccessor = new MessageSourceAccessor(context);
+			initApplicationContext(context);
+		} else {
+			// Ignore reinitialization if same context passed in.
+			if (this.applicationContext != context) {
+				throw new ApplicationContextException(
+						"Cannot reinitialize with different application context: current one is [" +
+								this.applicationContext + "], passed-in one is [" + context + "]");
+			}
+		}
+	}
+
+	/**
+	 * 子类可重写此方法
+	 *
+	 * @see #getApplicationContext
+	 * @see #getMessageSourceAccessor
+	 */
+	protected boolean isContextRequired() {
+		return false;
+	}
+
+	/**
+	 * 注入context的类型
+	 *
+	 * @see #setApplicationContext
+	 */
+	protected Class<?> requiredContextClass() {
+		return ApplicationContext.class;
+	}
+
+	/**
+	 * 父类的初始化方法，即在调用setApplicationContext()方法之后如果传入的context为空，则调用此方法自行初始化
+	 *
+	 * @param context the containing ApplicationContext
+	 * @throws ApplicationContextException in case of initialization errors
+	 * @throws BeansException              if thrown by ApplicationContext methods
+	 * @see #setApplicationContext
+	 */
+	protected void initApplicationContext(ApplicationContext context) throws BeansException {
+		initApplicationContext();
+	}
+
+	/**
+	 * 提供给子类重写的方法
+	 * {@link #initApplicationContext(org.springframework.context.ApplicationContext)}.
+	 *
+	 * @throws ApplicationContextException in case of initialization errors
+	 * @throws BeansException              if thrown by ApplicationContext methods
+	 * @see #setApplicationContext
+	 */
+	protected void initApplicationContext() throws BeansException {
+	}
+
+
+	/**
+	 * 获取ApplicationContext方法，子类通过此方法获取ApplicationContext对象
+	 *
+	 * @throws IllegalStateException if not running in an ApplicationContext
+	 */
+	@Nullable
+	public final ApplicationContext getApplicationContext() throws IllegalStateException {
+		if (this.applicationContext == null && isContextRequired()) {
+			throw new IllegalStateException(
+					"ApplicationObjectSupport instance [" + this + "] does not run in an ApplicationContext");
+		}
+		return this.applicationContext;
+	}
+
+	/**
+	 * 获取ApplicationContext方法，子类通过此方法获取ApplicationContext对象
+	 *
+	 * @return the ApplicationContext (never {@code null})
+	 * @throws IllegalStateException in case of no ApplicationContext set
+	 * @since 5.0
+	 */
+	protected final ApplicationContext obtainApplicationContext() {
+		ApplicationContext applicationContext = getApplicationContext();
+		Assert.state(applicationContext != null, "No ApplicationContext");
+		return applicationContext;
+	}
+
+	/**
+	 * Return a MessageSourceAccessor for the application context
+	 * used by this object, for easy message access.
+	 *
+	 * @throws IllegalStateException if not running in an ApplicationContext
+	 */
+	@Nullable
+	protected final MessageSourceAccessor getMessageSourceAccessor() throws IllegalStateException {
+		if (this.messageSourceAccessor == null && isContextRequired()) {
+			throw new IllegalStateException(
+					"ApplicationObjectSupport instance [" + this + "] does not run in an ApplicationContext");
+		}
+		return this.messageSourceAccessor;
+	}
+
+}
